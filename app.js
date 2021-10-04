0760dbe20c23d2d0b3417baea527c745fdc22a96
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-04 19:51:13 +0530

    promise

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-//window object
-console.log(this.alert('HI'))
+// //template
+// let car = {
+//     name: "C class",
+//     manufacture: "CAR",
+//     print: () => {
+//         console.log(`${car.name}was created  by${car.manufacture}`)
+//     }
+// }
+// car.print()
+
+//template
+// let car = {
+//     name: "C class",
+//     manufacture: "CAR",
+//     print: () => {
+//         console.log(`${car.name}was created  by${car.manufacture}`)
+//     }
+// }
+// console.log(car)
+//     // console.log(Object.values(car))
+//     // console.log(Object.keys(car))
+// console.log(car.hasOwnProperty)
+
+//promise
+// setTimeout(() => {
+//     console.log("hemanth")
+// }, 0)
+
+// console.log("Hi")
+// console.log('heee')
+var data = fetch('https://jsonplaceholder.tyicode.com/todos/10')
+console.log(data)
