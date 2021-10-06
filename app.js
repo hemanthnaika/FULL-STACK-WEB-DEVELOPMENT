7323e0ac86cd8763f64c6883907a3edfda4e9392
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-06 18:47:07 +0530

    Name In console

--- app.js
+++ app.js
@@ -1,32 +1,7 @@
-// //template
-// let car = {
-//     name: "C class",
-//     manufacture: "CAR",
-//     print: () => {
-//         console.log(`${car.name}was created  by${car.manufacture}`)
-//     }
-// }
-// car.print()
-
-//template
-// let car = {
-//     name: "C class",
-//     manufacture: "CAR",
-//     print: () => {
-//         console.log(`${car.name}was created  by${car.manufacture}`)
-//     }
-// }
-// console.log(car)
-//     // console.log(Object.values(car))
-//     // console.log(Object.keys(car))
-// console.log(car.hasOwnProperty)
-
-//promise
-// setTimeout(() => {
-//     console.log("hemanth")
-// }, 0)
-
-// console.log("Hi")
-// console.log('heee')
-var data = fetch('https://jsonplaceholder.tyicode.com/todos/10')
-console.log(data)
+// Name In console
+let Name = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Hemanth");
+    }, 3000);
+});
+Name.then(Name => { console.log(Name) });
