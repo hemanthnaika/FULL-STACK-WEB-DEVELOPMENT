e0f9b46efc2858c6ceeca966ba03b19885824b0f
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-09 13:47:48 +0530

    Take The Input From User

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-//!Creating Call Back function
-var arr = ['Go To GYM', 'Eat', 'House', 'Add']
-arr.forEach((Element, index) => {
-    console.log(Element, index)
-})
+//!Creating Add even listener And Take the input form the user
+var button = document.querySelector('button')
+var input = document.querySelector('input')
+var list = document.querySelector('ul')
+
+const callbackfunc = (event) => {
+    // console.log(event.target)
+    // console.log(input.value)
+    const user = input.value
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(user)
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
+button.addEventListener('click', callbackfunc)
