7aea376d479c662e766d6e6a9adbc66894ec6030
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-09 15:57:35 +0530

    calulator

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
-//!Add the Dark Mode
-var body = document.querySelector('body')
-var button = document.querySelector('button')
-const callbackfunc = (event) => {
-    body.classList.toggle('dark')
+//!Calculator
+const a = document.querySelectorAll('input')[0]
+const b = document.querySelectorAll('input')[1]
+const add = document.querySelector('#add')
+const sub = document.querySelector('#sub')
+const multiply = document.querySelector('#multiply')
+const resultBox = document.querySelector('.result')
+    // const sum = () => {
+    //     //!parseInt: it is connvert String into Number
+    //     const result = parseInt(a.value) + parseInt(b.value)
+    //     console.log(result)
+    //         //!To connvert result into Html page
+    //     resultBox.innerHTML = result
+    // }
+    // const subt = () => {
+    //     //!parseInt: it is connvert String into Number
+    //     const result = parseInt(a.value) - parseInt(b.value)
+    //         // console.log(result)
+    //         //         //!To connvert result into Html page
+    //     resultBox.innerHTML = result
+    // }
 
+// const multi = () => {
+//     //!parseInt: it is connvert String into Number
+//     const result = parseInt(a.value) * parseInt(b.value)
+//     console.log(result)
+//         //         //!To connvert result into Html page
+//     resultBox.innerHTML = result
+// }
+const calculator = (event, operation) => {
+    console.log(event.target)
+        // console.log(event)
+        // if (operation == 'add') {
+        //     console.log('adding')
+
+    // } else if (operation == 'multiply') {
+    //     console.log('multiply')
+    // }
+    switch (operation) {
+        case "add":
+            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
+            break
+        case "multiply":
+            resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)
+            break
+        case "sub":
+            resultBox.innerHTML = parseInt(a.value) - parseInt(b.value)
+            break
+        default:
+            resultBox.innerHTML = "Not A valid"
+    }
 }
-button.addEventListener('click', callbackfunc)
+
+add.addEventListener('click', (event) => calculator(event, 'add'))
+multiply.addEventListener('click', (event) => calculator(event, 'multiply'))
+sub.addEventListener('click', (event) => calculator(event, 'sub'))
