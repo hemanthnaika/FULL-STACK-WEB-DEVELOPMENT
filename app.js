7d9a08943089b4d610814b3cf4694d5af1680f9a
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-09 16:16:58 +0530

    splice

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
+//!Splice
 var arr = [1, 2, 3, 4, 5, 6, 7, 8]
-    //!Shift And UnShift
-    // arr.shift(6)
-    // console.log(arr)
-    //!Slice
-    // var new_arr = arr.slice(2, 4)
-    // console.log(new_arr)
-
-var new_arr = arr.filter((Element, index) => {
-    if (Element > 100) return true
-})
-console.log(new_arr)
+arr.splice(2, 2)
+console.log(arr)
