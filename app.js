1df8c1bb5c95dd178d94f34865177ba56aca2304
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-10 23:00:15 +0530

    To create A fetch API

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
-//!To create Promise
-var promise
-setTimeout(() => {
-    console.log("hemanth")
-}, 2000)
+//! Frist log the one, Two etc and next settimeout is log
+// console.log('one')
+// console.log('Two')
+//     //?To crate SetTimeout
+// setTimeout(() => {
+//     console.log('hi')
+// }, 5000)
+// console.log('Three')
+
+//!To create the Promise
+//?new= It is a Key word to create a new promosie
+// let value = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Hello')
+//     }, 3000)
+// })
+// console.log(value)
+// setTimeout(() => {
+//     console.log(value)
+// }, 4000)
+
+//! To create A Fetch API
+//!then is key word of callbackof
+//!data is not key word
+let response = fetch('https://jsonplaceholder.typicode.com/users').then(data => {
+    let parsedData = data.json()
+    console.log(parsedData)
+    return (parsedData)
+        //!book not a keyword
+}).then(book => { console.log(book) })
+
+
+// console.log(response)
+// setTimeout(() => {
+//     console.log(response)
+//     const data = response.json()
+//     console.log(data)
+// }, 2000)
