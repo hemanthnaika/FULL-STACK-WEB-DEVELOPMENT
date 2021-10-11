1a806a250ab735ec9753a1ed8ec2df7cf304eb7c
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-11 23:03:15 +0530

    recipe app First

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const getData = async(item) => {
     let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log()
-    data.hits.forEach(recipe => {
-        console.log(recipe)
-    })
+        // console.log()
+    console.log(data.hits)
 }
 button.addEventListener('click', (e) => {
     getData(input.value)
