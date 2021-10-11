3cc6510f69adc53bf217ed102cda146961268fb9
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-11 21:45:13 +0530

    Conver Data into async And Await

--- app.js
+++ app.js
@@ -1,40 +1,18 @@
-//!To crate The Data and display In Display
-const table = document.querySelector('table')
-const createRow = (user) => {
-    let newRow = document.createElement('tr')
-    let element = `  
-    <td>${user.id}</td>
-   <td>${user.name}</td>
-   <td>${user.email}</td>
-   <td>${user.Username}</td>`
-    newRow.innerHTML = element
-    return newRow
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//         data.forEach(user => {
+
+//             console.log(data)
+
+//         })
+//     })
+//!To conver Data into async And Await is only work in async
+const fetchData = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
 }
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        data.forEach(user => {
-
-            table.appendChild(createRow(user))
-
-        })
-    })
-
-
-
-
-
-
-
-
-// .then(resp=>resp.json()).then(data=>{
-//     console.log(data)
-//     append_data=(element)=>{
-//         lielement=document.createElement("li")
-//         user_detail=element['name']+element['username']+element['email']
-//     textNode=document.createTextNode(user_detail)
-//     lielement.appendChi
-//     }
-// })
-// const list = document.querySelector('ul')
+fetchData()
