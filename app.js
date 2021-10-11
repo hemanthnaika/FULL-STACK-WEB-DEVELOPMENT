0591a41bccce2f99bbc2c875626cd1144a2ebbbd
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-11 13:37:56 +0530

    To take the input form API and display in window To ease And Implemention

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 //!To crate The Data and display In Display
 const table = document.querySelector('table')
-console.log(table)
+const createRow = (user) => {
+    let newRow = document.createElement('tr')
+    let element = `  
+    <td>${user.id}</td>
+   <td>${user.name}</td>
+   <td>${user.email}</td>
+   <td>${user.Username}</td>`
+    newRow.innerHTML = element
+    return newRow
+}
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(data => {
         console.log(data)
         data.forEach(user => {
-            let newRow = document.createElement('tr')
-            let element = `  
-            <td>${user.id}</td>
-           <td>${user.name}</td>
-           <td>${user.email}</td>
-           <td>${user.Username}</td>`
-            newRow.innerHTML = element
 
-            table.appendChild(newRow)
+            table.appendChild(createRow(user))
 
         })
     })
