7ed7299bf6436a23a5f25730cba26fa57326f6b8
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-11 13:32:42 +0530

    To take the input form API and display in window

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-let Name = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("HELLO")
-        }, 3000)
+//!To crate The Data and display In Display
+const table = document.querySelector('table')
+console.log(table)
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(user => {
+            let newRow = document.createElement('tr')
+            let element = `  
+            <td>${user.id}</td>
+           <td>${user.name}</td>
+           <td>${user.email}</td>
+           <td>${user.Username}</td>`
+            newRow.innerHTML = element
+
+            table.appendChild(newRow)
+
+        })
     })
-    //!Data Not a Key Word
-Name.then(data => { console.log(data) })
+
+
+
+
+
+
+
+
+// .then(resp=>resp.json()).then(data=>{
+//     console.log(data)
+//     append_data=(element)=>{
+//         lielement=document.createElement("li")
+//         user_detail=element['name']+element['username']+element['email']
+//     textNode=document.createTextNode(user_detail)
+//     lielement.appendChi
+//     }
+// })
+// const list = document.querySelector('ul')
