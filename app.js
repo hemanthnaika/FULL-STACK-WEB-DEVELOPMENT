01995b56c87e0d6de806701a7075d3798a95e2a0
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-11 12:48:33 +0530

    To crate Promise Implemation

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-//!To create fetch API
-// let response = fetch("https://jsonplaceholder.typicode.com/users")
-//     .then((res) => res.json())
-//     .then((data) => {
-//         console.log(data)
-//     })
-
-//!To create Which user click in console
-const list = document.querySelector('ul')
-let arr = ['one', 'Two', 'Three', 'Four']
-    // console.log(list)
-
-arr.forEach(chore => {
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(chore)
-    element.appendChild(textNode)
-    element.addEventListener('click', (e) => {
-        //!To Give the Prompt To user Are sure
-        const userInput = window.prompt(`Are you sure that you want to delete "${chore}"`)
-        if (userInput == 'YES') e.target.remove()
-            // console.log(e.target.innerHTML)
-            //     //!To Remove An Element
-            // e.target.remove()
+let Name = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("HELLO")
+        }, 3000)
     })
-    list.appendChild(element)
-
-})
+    //!Data Not a Key Word
+Name.then(data => { console.log(data) })
