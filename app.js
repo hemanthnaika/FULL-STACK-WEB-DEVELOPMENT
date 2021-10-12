5919092c00f969961bc2ac6f77615757a8228d1f
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-12 15:07:57 +0530

    To do list localStorge

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //     //!To clear The localStorage
 // localStorage.clear
-
 const button = document.querySelector('button')
 const input = document.querySelector('input')
 const div = document.querySelector('div')
-const arr = [1, 2, 3, 4, 5]
-    // console.log(arr)
-    //     //!Arr convert into string Use stringify key
-    // console.log(JSON.stringify(arr))
-localStorage.setItem('val', JSON.stringify(arr))
-    //!To convert string into arr use JSON.parse key
-console.log(JSON.parse(localStorage.getItem('val')))
+    //!To get input when we reload and It show preys
+div.innerHTML = localStorage.getItem('InputValue')
+button.addEventListener('click', (req, res) => {
+    localStorage.setItem('InputValue', input.value)
+    div.innerHTML = input.value
+
+
+})
