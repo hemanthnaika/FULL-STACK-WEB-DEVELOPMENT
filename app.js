929ffafe6655a488fcbcce787f90d58e2fcf2b7b
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-12 15:06:32 +0530

    Conver arr into string and Convert into arr

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-//!To input The value into LocalStorage
-// localStorage.setItem(`InputValue`, "something")
-//!To get the localStorage Value
-console.log(localStorage.getItem("InputValue"))
+//     //!To clear The localStorage
+// localStorage.clear
+
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
+const arr = [1, 2, 3, 4, 5]
+    // console.log(arr)
+    //     //!Arr convert into string Use stringify key
+    // console.log(JSON.stringify(arr))
+localStorage.setItem('val', JSON.stringify(arr))
+    //!To convert string into arr use JSON.parse key
+console.log(JSON.parse(localStorage.getItem('val')))
