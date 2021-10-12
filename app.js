36970e1a4e1c56e348cdc73d059d231e1a54c0f0
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-12 15:44:03 +0530

    Resolve And Relative Path

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-//     //!To clear The localStorage
-// localStorage.clear
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
-    //!To get input when we reload and It show preys
-div.innerHTML = localStorage.getItem('InputValue')
-button.addEventListener('click', (req, res) => {
-    localStorage.setItem('InputValue', input.value)
-    div.innerHTML = input.value
+//?Resolve
+// const path = require('path')
+//!To find the Path relative path into absolute path Ex \user\desktop\  into E:\user\desktop
+//!The key word is resolve
+// console.log(path.resolve('./app.js'))
 
-
-})
+//?Relative
+const path = require('path')
+    //!To find the Path relative path Ex E:\user\desktop into css\temp\utils.js
+    //!The key word is relative
+console.log(path.relative('../', "./temp/utils.js"))
