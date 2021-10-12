14288c7c84a549e0762c07ec13d8e3c39233673c
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-12 16:47:33 +0530

    Npm initialse

--- temp/utils.js
+++
@@ -1,4 +0,0 @@
-const path = require('path')
-    //!To find the Path relative path into absolute path Ex \user\desktop\  into E:\user\desktop
-    //!The key word is resolve
-console.log(path.resolve('../', "./temp/utils.js"))
