133bef97100743fdb5f0487cb1dfd7424275c5c7
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-12 12:05:33 +0530

    module

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
-const input = document.querySelector('input')
-const button = document.querySelector('button')
-const parent = document.querySelector('div.grid')
-    // console.log(parent)
-const getData = async(item) => {
-    console.log(item)
-    const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
-    console.log(url)
-    const res = await fetch(url)
-    const data = await res.json()
-        // console.log()
-        // console.log(data.hits)
-    const recipies = data.hits
-    recipies.forEach(item => {
-        const { recipe } = item
-        console.log(item)
-        console.log(item.recipies)
-        const { label, image, source, calories } = recipe
+// console.log(module)
+//!Imports using require 
+// console.log(require('./index'))
+//! To create Import require is key word
+// const stuff = require('./index')
+// console.log(stuff)
 
-        const template = `<img class = "card__image"
-        src = "${image}" / >
-        <div class = "card__data" >
-        <div class = "card__info" >
-        <h2>${label}</h2> 
-            <p>From ${source}</p> 
-       </div> 
-    <h3 class = "card__price" >${calories.toFixed(2)}</h3> 
-    <button class = "card__add" > + < /button> </div >`
-        const newCard = document.createElement('article')
-        newCard.setAttribute('class', 'card')
-            //!Set Attribute is use to add style and etc
-        newCard.setAttribute('style', 'color:cyan')
-        newCard.setAttribute('style', 'margin-bottom:50px')
-        newCard.innerHTML = template
-        parent.appendChild(newCard)
-    })
 
-}
-button.addEventListener('click', (e) => {
-    getData(input.value)
-})
+//!To import from utils.js
+const { add, subtract, multiply, divide, message } = require('./utils')
+
+add()
+subtract()
+multiply()
+divide()
+
+console.log(message)
---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log('Something')
+// for (var i = 0; i < 10; i++) { 
+//     console.log('iteration', i + 1)
+// }
+//!Module
+// const greet = () => 'hi'
+//     // module.exports.something = "hi"
+//     //!Exports
+// module.exports.greet = greet
+//     // console.log(module)
