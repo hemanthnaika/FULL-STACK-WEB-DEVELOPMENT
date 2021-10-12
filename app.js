40c0c5bde7607cfee6c8962c7f42829d78732ecc
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-12 09:56:54 +0530

    destructuring

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-const input = document.querySelector('input')
-const button = document.querySelector('button')
-const getData = async(item) => {
-    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
-    const res = await fetch(url)
-    const data = await res.json()
-        // console.log()
-    console.log(data.hits)
-}
-button.addEventListener('click', (e) => {
-    getData(input.value)
+// const input = document.querySelector('input')
+// const button = document.querySelector('button')
+// const parent = document.querySelector('div.grid')
+//     // console.log(parent)
+// const getData = async(item) => {
+//     console.log(item)
+//     const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+//     console.log(url)
+//     const res = await fetch(url)
+//     const data = await res.json()
+//         // console.log()
+//         // console.log(data.hits)
+//     const recipies = data.hits
+//     recipies.forEach(item => {
+//         console.log(item)
+//         console.log(item.recipies)
+//         const template = `<img class = "card__image"
+//         src = "https://i.ibb.co/RT0bjJq/food1.png" / >
+//         <div class = "card__data" >
+//         <div class = "card__info" >
+//         <h2> Nombre Comida </h2> 
+//             <p>Descripcion de la comida, con ingredientes </p> 
+//        </div> 
+//     <h3 class = "card__price" > $7 .50 </h3> 
+//     <button class = "card__add" > + < /button> </div >`
+//         const newCard = document.createElement('article')
+//         newCard.setAttribute('class', 'card')
+//             //!Set Attribute is use to add style and etc
+//         newCard.setAttribute('style', 'color:cyan')
+//         newCard.setAttribute('style', 'margin-bottom:40px')
+//         newCard.innerHTML = template
+//         parent.appendChild(newCard)
+//     })
+
+// }
+// button.addEventListener('click', (e) => {
+//     getData(input.value)
+// })
+
+//!Destacture
+const person = ({
+    name: "Hemanth",
+    age: 19
 })
+const { name, age } = person
+console.log(name, age)
