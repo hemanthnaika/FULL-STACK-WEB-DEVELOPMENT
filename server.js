0f77f0934bc86e797c84a70c508ad6272ec3bc95
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-13 19:57:37 +0530

    Is Admin

--- server.js
+++ server.js
@@ -1,54 +1,20 @@
-// //!To create server using Express
-// const express = require('express')
-// const PORT = 3000
-// const app = express()
-//     // console.log(express)
-//     // app.post('/', (req, res) => {
-//     //     res.send("Server is running")
-//     // })
-
-// // app.post('/', (req, res) => {
-// //     res.send("login From")
-// // })
-// // app.patch('/', (req, res) => {
-// //     res.send("login From")
-// // })
-// // app.delete('/', (req, res) => {
-// //     res.send("login From")
-// // })
-
-// const verify = (req, res, next) => {
-//         if (req.headers['user-agent'] === "http://localhost:3000/"
-//             next()
-//             else res.send("Blocked")
-//         }
-//   app.get('/', verify, (req, res) => {
-//             res.send("HIII")
-//      })
-
-//         // app.get('/', (req, res) => {
-//         //             // console.log(req.headers.['user-agent'])
-//         //             if (req.headers['user-agent'] === ("thander") res.send('GET')
-//         //                 else res.send("Blocked")
-
-//         //             }) 
-//         app.listen(PORT, () => {
-//             console.log(`Server Listening at port `)
-//         })
-
 const express = require('express')
 const PORT = 3000
 const app = express()
 
-
-const verify = (req, res, next) => {
-    if (req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next()
-    else res.send("BLOCKED")
+const isAdmin = (req, res, next) => {
+    if (req.headers.admin === 'true') next()
+    else res.send("UNAUTHORISED")
 }
 
-app.get('/', verify, (req, res) => {
-    res.send('VERIFIED')
 
+app.get('/public', (req, res) => {
+    console.log(req.headers)
+    res.send(`I'm a public route`)
+})
+
+app.get('/private', isAdmin, (req, res) => {
+    res.send(`I'm a admin route`)
 })
 
 
