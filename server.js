c1b97f080b8c533ed7c2d69202e8642988e774b5
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-14 13:33:49 +0530

    POST THE JSON OBJECT OR REQUEST IN TC

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
-//! TO CREATE AUTHORIZE UNAUTHORIZE OR  ADMIN USING THUNDER CLIENT
-//!IN HEADER IS ADMIN OR NOT
+//!To send the POST USING JSON OBJECT
 const express = require('express')
 const PORT = 3000
 const app = express()
+    //!Express convert into stream using express.json
+app.use(express.json())
 
-const isAdmin = (req, res, next) => {
-    if (req.headers.admin === 'true') next()
-    else res.send("UNAUTHORIZE")
-}
-app.get('/public', (req, res) => {
-    console.log(req.headers)
-    res.send(`I'm public rout`)
-})
-
-app.get('/private', isAdmin, (req, res) => {
-    res.send(`I'm Admin rout`)
+app.post('/signup', (req, res) => {
+    console.log(req.body)
+    res.send("Successfully")
 })
 
 app.listen(PORT, () => {
