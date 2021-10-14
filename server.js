fa32066be364bf44f9946ad5fab53a715471d4dd
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-14 11:38:10 +0530

    To create get and post request

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+//!How to manke get and post request
 const express = require('express')
 const PORT = 3000
 const app = express()
 
-app.unsubscribe(express.json())
-app.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send('daaaaaa')
+app.get('/', (req, res) => {
+    res.send('Server is running')
 })
 
+app.post('/', (req, res) => {
+    res.send('login Form')
+})
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
