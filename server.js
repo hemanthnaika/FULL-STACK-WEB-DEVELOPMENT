4bce60ff76aaf4f4a228ea4dcaffb0cca23830cd
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-14 11:45:31 +0530

    How to create get post delet and patch

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-//!How to manke get and post request
+//!How to manke get post patch delete request
 const express = require('express')
 const PORT = 3000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Server is running')
+    res.send('Server is GET')
 })
 
 app.post('/', (req, res) => {
-    res.send('login Form')
+    res.send('Server is POST')
+})
+
+
+
+app.delete('/', (req, res) => {
+    res.send('Server is DELETE')
+})
+
+app.patch('/', (req, res) => {
+    res.send('Server is PATCH')
 })
 
 app.listen(PORT, () => {
