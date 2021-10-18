0d6437173c9a9af447bb6cd7383b18f0446afe24
hemanthnaika <hemanthnaika22@gmail.com>
2021-10-18 18:23:07 +0530

    Add the database

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-//!To send the POST USING JSON OBJECT
 const express = require('express')
-const PORT = 3000
+const database = require('./database/db')
 const app = express()
-    //!Express convert into stream using express.json
-app.use(express.json())
+app.use(express.json)
 
-app.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send("Successfully")
-})
 
-app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`)
+
+app.listen(3001, () => {
+    console.log("Server started at port 3001")
 })
